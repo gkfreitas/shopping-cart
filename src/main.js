4e914036b5ce291ccc22aca89df808b1442053c5
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-16 18:29:07 -0300

    REQ4 - Finalizada, adicionado texto de carregando durante a requisição da API

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const loadingElement = document.querySelector('.loading');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const createProducts = async () => {
   const sectionProducts = document.querySelector('.products');
@@ -10,5 +11,6 @@ const createProducts = async () => {
   product.forEach((e) => {
     sectionProducts.appendChild(createProductElement(e));
   });
+  loadingElement.parentNode.removeChild(loadingElement);
 };
 createProducts();
