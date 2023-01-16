8abb739f5e87abf4722fab935dcd897a2e11b697
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-16 15:59:28 -0300

    Testes do requisito 1 adicionados

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,17 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+function testFetchProductsList(search) {
+  if (search === undefined) {
+    throw new Error('Termo de busca não informado');
+  }
+}
+
+export const fetchProductsList = (search) => {
   // seu código aqui
+  try {
+    testFetchProductsList(search);
+  } catch (error) {
+    return error.message;
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    const expected = 'function';
+    expect(typeof fetchProductsList).toBe(expected);
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    const expected = 'object';
+    expect(typeof fetchProductsList('computador')).toBe(expected);
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const expected = 'computador';
+    expect(await fetchProductsList('computador').query).toEqual(expected);
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    expect(await fetchProductsList('computador').results[0]).toEqual(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro ', async () => {
+    const expected = new Error('Termo de busca não informado');
+    expect(await fetchProductsList()).toThrow(expected);
+  });
 });
