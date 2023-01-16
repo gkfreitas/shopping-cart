46690bd2361da493f99391c002ed750645bac814
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-16 18:20:37 -0300

    Requisito 3 finalizado, produtos adicionado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const createProducts = async () => {
+  const sectionProducts = document.querySelector('.products');
+  const product = await fetchProductsList('computador');
+  product.forEach((e) => {
+    sectionProducts.appendChild(createProductElement(e));
+  });
+};
+createProducts();
