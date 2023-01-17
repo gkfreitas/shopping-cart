eeba7a3b3a914cd9e018ade4afbbb0e100d3c049
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-01-17 13:19:11 -0300

    REQ8 - Finalizado, função para adicionar produtos ao carrinho de compras criada.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
+import { saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 const loadingElement = document.querySelector('.loading');
 const sectionProducts = document.querySelector('.products');
 const errorElement = document.querySelector('.error');
+const cartProducts = document.querySelector('.cart__products');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const createProducts = async (search) => {
-  const product = await fetchProductsList(search);
-  product.forEach((e) => {
-    sectionProducts.appendChild(createProductElement(e));
-  });
-  loadingElement.parentNode.removeChild(loadingElement);
+  try {
+    const product = await fetchProductsList(search);
+    product.forEach((e) => {
+      sectionProducts.appendChild(createProductElement(e));
+    });
+    loadingElement.parentNode.removeChild(loadingElement);
+    errorElement.parentNode.removeChild(errorElement);
+  } catch (error) {
+    errorElement.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  }
+};
+const createProdcutsCart = async () => {
+  await createProducts('computador');
+  const addCart = document.querySelectorAll('.product__add');
+  const productId = document.querySelectorAll('.product__id');
+  addCart.forEach((e, i) => e.addEventListener('click', async () => {
+    const id = productId[i].innerHTML;
+    saveCartID(id);
+    const data = await fetchProduct(id);
+    const cartProduct = createProductElement(data);
+    cartProducts.appendChild(cartProduct);
+  }));
 };
 
-try {
-  await createProducts('computador');
-  errorElement.parentNode.removeChild(errorElement);
-} catch (error) {
-  errorElement.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
-}
+createProdcutsCart();
